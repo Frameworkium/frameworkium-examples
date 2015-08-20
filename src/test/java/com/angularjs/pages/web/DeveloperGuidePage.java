e5f557046bfe3aadc5d28dc7a1a939137f9e6773
jleeh <JonnyH@outlook.com>
2015-08-20 14:35:46 +0100

    Updated to use 1.1.2 of frameworkium-core. Optimised imports.

--- src/test/java/com/angularjs/pages/web/DeveloperGuidePage.java
+++ src/test/java/com/angularjs/pages/web/DeveloperGuidePage.java
@@ -2,12 +2,9 @@ package com.angularjs.pages.web;
 
 import com.frameworkium.pages.internal.BasePage;
 import com.frameworkium.pages.internal.Visible;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import ru.yandex.qatools.htmlelements.annotations.Name;
-
-import static com.paulhammant.ngwebdriver.WaitForAngularRequestsToFinish.waitForAngularRequestsToFinish;
 
 public class DeveloperGuidePage extends BasePage<DeveloperGuidePage> {
 
@@ -31,7 +28,7 @@ public class DeveloperGuidePage extends BasePage<DeveloperGuidePage> {
 
     public void clickBootstrapSearchItem() {
         bootstrapSearchItem.click();
-        waitForAngularRequestsToFinish((JavascriptExecutor) driver);
+        waitForAngularRequestsToFinish();
     }
 
     public String getGuideTitle() {

ccf050b9c4b6553739edc250bb27336eab9bd62b
Graham Russell <graham@ham1.co.uk>
2016-06-12 11:37:48 +0100

    Fixed Search Test - also made it use streams to improve performance.

--- src/test/java/com/google/pages/web/ResultsPage.java
+++ src/test/java/com/google/pages/web/ResultsPage.java
@@ -2,27 +2,36 @@ package com.google.pages.web;
 
 import com.frameworkium.core.ui.annotations.Visible;
 import com.frameworkium.core.ui.pages.BasePage;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import ru.yandex.qatools.allure.annotations.Step;
 import ru.yandex.qatools.htmlelements.annotations.Name;
+import ru.yandex.qatools.htmlelements.element.Link;
+import ru.yandex.qatools.htmlelements.element.TextBlock;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ResultsPage extends BasePage<ResultsPage> {
 
-    @Name("Search result titles")
     @Visible
-    @FindBy(css = ".rc .r a")
-    private List<WebElement> resultTitles;
+    @Name("Search results stats")
+    @FindBy(id = "resultStats")
+    private TextBlock resultStats;
 
+    @Name("Search result titles")
+    @FindBy(css = "h3.r > a")
+    private List<Link> resultTitles;
+
+    /**
+     * Uses a stream to lazily iterate through links. We expect the first result
+     * to be the one we want, using a Stream means we won't have to build an
+     * entire list which is relatively expensive.
+     */
     @Step("Get search result titles")
-    public List<String> getResultTitles() {
+    public Stream<String> getResultTitles() {
 
         return resultTitles.stream()
-                .map(WebElement::getText)
-                .collect(Collectors.toList());
+                .map(Link::getText);
     }
 
 }
--- src/test/java/com/google/tests/web/SearchTest.java
+++ src/test/java/com/google/tests/web/SearchTest.java
@@ -11,15 +11,16 @@ import static com.google.common.truth.Truth.assertThat;
 public class SearchTest extends BaseTest {
 
     @TestCaseId("FT-1")
-    @Test(description = "Run a search on google and check result returned")
+    @Test(description = "Run a search on Google and check result returned")
     public void searchTest() {
 
-        //Navigate to google and run a search
+        // Navigate to google and run a search
         ResultsPage resultsPage = HomePage.open().then().runSearch("Hello World");
 
-        //Check that the results contains the expected result
-        assertThat(resultsPage.getResultTitles())
-                .contains("\"Hello, World!\" program - Wikipedia, the free encyclopedia");
-
+        // Check that the results contains the expected result
+        String expectedTitle = "\"Hello, World!\" program - Wikipedia, the free encyclopedia";
+        assertThat(
+                resultsPage.getResultTitles().anyMatch(expectedTitle::equals))
+                .isTrue();
     }
 }

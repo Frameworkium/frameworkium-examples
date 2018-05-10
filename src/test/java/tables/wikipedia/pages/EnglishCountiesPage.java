f3e8739b81c27d1dd0a93cb9e40e50b8cf820390
Graham Russell <graham@ham1.co.uk>
2018-05-10 15:12:55 +0100

    Added StreamTable, with two tests, one with headers using col-span.

---
+++ src/test/java/tables/wikipedia/pages/EnglishCountiesPage.java
@@ -0,0 +1,39 @@
+package tables.wikipedia.pages;
+
+import com.frameworkium.core.ui.annotations.Visible;
+import com.frameworkium.core.ui.pages.BasePage;
+import com.frameworkium.core.ui.pages.PageFactory;
+import org.openqa.selenium.NotFoundException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import tables.wikipedia.StreamTable;
+
+import java.util.stream.Stream;
+
+public class EnglishCountiesPage extends BasePage<EnglishCountiesPage> {
+
+    @Visible
+    @CacheLookup
+    @FindBy(css = "table.wikitable") // luckily there's only one
+    private StreamTable listTable;
+
+    public static EnglishCountiesPage open() {
+        return PageFactory.newInstance(EnglishCountiesPage.class,
+                "https://en.wikipedia.org/wiki/List_of_ceremonial_counties_of_England");
+    }
+
+    public int populationOf(String countyName) {
+        String population = listTable.getCellsByLookup("Population", "County", countyName)
+                .findFirst()
+                .orElseThrow(NotFoundException::new)
+                .getText();
+        return Integer.parseInt(population.replaceAll(",", ""));
+    }
+
+    public Stream<Integer> densities() {
+        return listTable.getColumn(e -> e.getText().startsWith("Density"))
+                .map(WebElement::getText)
+                .map(density -> Integer.parseInt(density.replaceAll(",", "")));
+    }
+}
---
+++ src/test/java/tables/wikipedia/pages/HighestMountainPage.java
@@ -0,0 +1,62 @@
+package tables.wikipedia.pages;
+
+import com.frameworkium.core.ui.annotations.Visible;
+import com.frameworkium.core.ui.pages.BasePage;
+import com.frameworkium.core.ui.pages.PageFactory;
+import org.openqa.selenium.NotFoundException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import tables.wikipedia.StreamTable;
+
+import static java.util.stream.Collectors.toList;
+
+public class HighestMountainPage extends BasePage<HighestMountainPage> {
+
+    @Visible
+    @CacheLookup
+    @FindBy(css = "table.wikitable") // luckily there's only one
+    private StreamTable listTable;
+
+    public static HighestMountainPage open() {
+
+        return PageFactory.newInstance(HighestMountainPage.class,
+                "https://en.wikipedia.org/wiki/List_of_highest_mountains");
+    }
+
+    public String getRankByName(String mountainName) {
+        return getText(mountainName, 0);
+    }
+
+
+    public String getFirstAscentByName(String mountainName) {
+        return getText(mountainName, 8);
+    }
+
+    private String getText(String mountainName, int index) {
+        return listTable.getRows()
+                .map(row -> row.limit(index + 2).collect(toList()))
+                .filter(row -> row.get(1).getText().contains(mountainName))
+                .findFirst()
+                .orElseThrow(NotFoundException::new)
+                .get(index)
+                .getText();
+    }
+
+    public long mountainsHigherThan(int heightInMeters) {
+        return listTable.getColumn(2)
+                .map(WebElement::getText)
+                .map(this::parseHeight)
+                .filter(meters -> meters > heightInMeters)
+                .count();
+    }
+
+    /* Private (helper) methods */
+
+    private int parseHeight(String text) {
+        String cleansedText = text
+                .replace(",", "")
+                .replaceAll("\\[\\d\\]", "");
+        return Integer.parseInt(cleansedText);
+    }
+}

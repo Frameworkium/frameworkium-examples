fbe8557db8851798da6a9270b8f603cc5e76cb5e
MJL33 <mjlee.1@gmail.com>
2021-09-27 12:10:37 +0800

    Update tests

--- src/test/java/seleniumhq/components/HeaderComponent.java
+++ src/test/java/seleniumhq/components/HeaderComponent.java
@@ -1,7 +1,9 @@
 package seleniumhq.components;
 
+import com.frameworkium.core.ui.UITestLifecycle;
 import com.frameworkium.core.ui.pages.PageFactory;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
 import ru.yandex.qatools.htmlelements.element.Link;
 import seleniumhq.pages.SeleniumDownloadPage;
@@ -13,6 +15,7 @@ public class HeaderComponent extends HtmlElement {
     private Link downloadLink;
 
     public SeleniumDownloadPage clickDownloadLink() {
+        UITestLifecycle.get().getWait().until(ExpectedConditions.elementToBeClickable(downloadLink));
         downloadLink.click();
         return PageFactory.newInstance(SeleniumDownloadPage.class);
     }

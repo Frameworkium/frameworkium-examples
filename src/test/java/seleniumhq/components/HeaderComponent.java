e6c668424bc41337458397305b17b461e260e957
MJ <Mjl33@users.noreply.github.com>
2021-09-27 12:25:38 +0800

    Update tests (#18)
    * Update tests
    
    * Update tests
    
    * Update tests

--- src/test/java/seleniumhq/components/HeaderComponent.java
+++ src/test/java/seleniumhq/components/HeaderComponent.java
@@ -1,18 +1,21 @@
 package seleniumhq.components;
 
+import com.frameworkium.core.ui.UITestLifecycle;
 import com.frameworkium.core.ui.pages.PageFactory;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
 import ru.yandex.qatools.htmlelements.element.Link;
 import seleniumhq.pages.SeleniumDownloadPage;
 
-@FindBy(id = "header")
+@FindBy(className = "navbar")
 public class HeaderComponent extends HtmlElement {
 
-    @FindBy(css = "#navbar > a:nth-child(3)")
+    @FindBy(css = "#main_navbar [href='/downloads']")
     private Link downloadLink;
 
     public SeleniumDownloadPage clickDownloadLink() {
+        UITestLifecycle.get().getWait().until(ExpectedConditions.elementToBeClickable(downloadLink));
         downloadLink.click();
         return PageFactory.newInstance(SeleniumDownloadPage.class);
     }
